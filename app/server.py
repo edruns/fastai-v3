09706631e3a9eb086e7c84b6473d54d196d0ff8c
edruns <58447906+edruns@users.noreply.github.com>
2019-12-08 15:07:46 -0500

    Swapped bears for triathlon
    Removed bears and added swim, bike, run

--- app/server.py
+++ app/server.py
@@ -9,10 +9,10 @@ from starlette.middleware.cors import CORSMiddleware
 from starlette.responses import HTMLResponse, JSONResponse
 from starlette.staticfiles import StaticFiles
 
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://www.dropbox.com/s/t7sj8b45cn43pkv/export.pkl?dl=1'
 export_file_name = 'export.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['swim', 'bike', 'run']
 path = Path(__file__).parent
 
 app = Starlette()
